ef516acf2982bd44a05e554b87638e7f7265c947
rogerbf <roger@bergfalk.nu>
2017-10-26 23:51:31 +0200

    fix incorrect degree addition

--- source/index.js
+++ source/index.js
@@ -1,6 +1,12 @@
 import Blake2s from 'blake2s-js'
 import chroma from 'chroma-js'
 
+export const addDegrees = (a, b) => {
+  var output = (a + b) % 360
+  if (output < 0) output += 360
+  return output
+}
+
 export const stringToUint8Array = (input = ``) => {
   const source = unescape(encodeURIComponent(input))
   const output = new Uint8Array(source.length)
@@ -15,6 +21,6 @@ export const colorHash = (input = ``) =>
 
 export const complementary = (hex) => {
   const [ hue, saturation, lightness ] = chroma(hex).hsl()
-  const complementary = [ Math.abs(hue - 180), saturation, lightness ]
+  const complementary = [ addDegrees(180, hue), saturation, lightness ]
   return chroma.hsl(...complementary).hex()
 }
